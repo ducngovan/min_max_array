9eac78e6a533802a5022ed4b95ed154eba12a457
ngoduc <ngoducnk@gmail.com>
2019-10-03 10:28:15 +0700

    tim min max trong array

---
+++ max_min_array/javascript.js
@@ -0,0 +1,5 @@
+var arrray = [-3,5,1,3,2,10];
+var max_array=Math.max.apply(null,arrray);
+var min_array=Math.min.apply(null,arrray);
+document.write("gia tri max: "+max_array+"<br/>");
+document.write("gi tri min: "+min_array);
